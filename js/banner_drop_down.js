c482bffd4cf60b448111f095eaea513a268d3260
ByteCoder26 <tomykouk@gmail.com>
2025-12-01 18:02:39 +0000

    completed client's requirements

---
+++ js/banner_drop_down.js
@@ -0,0 +1,12 @@
+window.addEventListener("load", () => {
+    const banner = document.getElementById("shareOwnerBanner");
+    const closeBtn = document.getElementById("bannerClose");
+
+    setTimeout(() => {
+        banner.style.top = "0"; 
+    }, 400);
+
+    closeBtn.addEventListener("click", () => {
+        banner.style.top = "-120px";
+    });
+});
